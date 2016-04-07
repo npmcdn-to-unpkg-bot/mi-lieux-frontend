34f8e0fee62aef010c19c566df8d8c89ae9bc1c4
Nicolas Tournier <nicolas.tournier@team.telstra.com>
2016-04-07 23:56:39 +1000

    Reorganise folders and references WIP build scripts

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -1,4 +1,4 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {AppRootComponent} from './app.milieuxroot';
+import {AppRootComponent}} from './app.milieuxroot';
 
 bootstrap(AppRootComponent);
